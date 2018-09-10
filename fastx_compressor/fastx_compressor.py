5810d4ad962aff40034f6944d671d111d80fde81
VincentPreikstas <43098530+VincentPreikstas@users.noreply.github.com>
2018-09-09 22:18:10 -0400

    consolidated things and changed whitespace handling

--- fastx_compressor/fastx_compressor.py
+++ fastx_compressor/fastx_compressor.py
@@ -2,7 +2,7 @@
 
 """Main module."""
 
-"VERSION 1.2"
+"VERSION 1"
 
 def block_fastq_file(fastq_file, block_size=1000):
     blocks = ["", "", "", ""]
@@ -16,97 +16,7 @@ def block_fastq_file(fastq_file, block_size=1000):
     for temp in blocks:
         finishedString += "\n" + temp
 
-    finishedString.lstrip()
+    finishedString.strip()
 
 
     return finishedString
-
-
-"VERSION 1.1.1"
-
-"""
-
-def block_fastq_file(fastq_file, block_size=1000):
-    blocks = ["", "", "", ""]
-    counter = 0
-    (fastq_file) as x:
-        for line in x:
-            if counter != 3:
-                blocks[counter] = blocks[counter] + \n + line
-                counter ++
-            else:
-                blocks[counter] = blocks[counter] + \n + line
-                counter = 0
-    finishedString = ""
-    for temp in blocks:
-        finishedString = finishedString + \n + temp
-
-    finishedString.lstrip()
-
-
-    return finishedString
-"""
-
-"VERSION 1.1"
-"""
-def block_fastq_file(fastq_file, block_size=1000):
-    blocks = ["", "", "", ""]
-    counter = 0
-    (fastq_file) as x:
-        for line in x:
-            if counter == 0:
-                blocks[counter] = blocks[counter] + \n + line
-                counter ++
-            elif counter == 1:
-                blocks[counter] = blocks[counter] + \n + line
-                counter++
-            elif counter == 2:
-                blocks[counter] = blocks[counter] + \n + line
-                counter++
-            else:
-                blocks[counter] = blocks[counter] + \n + line
-                counter = 0
-    finishedString = ""
-    for temp in blocks:
-        finishedString = finishedString + \n + temp
-
-    finishedString.lstrip()
-
-
-    return finishedString
-
-"""
-
-"VERSION 1"
-
-"""
-
-def block_fastq_file(fastq_file, block_size=1000):
-    firstString = ""
-    secondString = ""
-    thirdString = ""
-    fourthString = ""
-    counter = 0
-    with open(fastq_file) as x:
-        for line in x:
-            if counter == 0:
-                firstString = firstString + \n + line
-                counter++
-            elif counter == 1:
-                secondString = secondString + \n + line
-                counter++
-            elif counter == 2:
-                thirdString = thirdString + \n + line
-                counter++
-            else:
-                fourthString = fourthString + \n + line
-                counter = 0
-    finishedString = firstString + \n + secondString + \n + thirdString + \n + fourthString
-
-    return finishedString
-
-"""         
-
-
-
-"Yield lines from a blocked fastq file starting with the header line."
