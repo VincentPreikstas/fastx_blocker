d93474871f9b7d592315d7a078edf675e5338ce5
VincentPreikstas <43098530+VincentPreikstas@users.noreply.github.com>
2018-09-18 20:52:49 -0400

    updated to properly format blocks

--- fastx_compressor/cli.py
+++ fastx_compressor/cli.py
@@ -10,10 +10,11 @@ from .fastx_compressor import block_fastq_file
 @click.command()
 @click.argument('block_size', type=int)
 @click.argument('fastx_file', type=click.File('r'))
-def main(block_size, fastx_file):
-    print(block_fastq_file(fastx_file, block_size))
+def main(fastx_file, block_size=1000):
+    for block in block_fastq_file(fastx_file, block_size):
+        print(block)
     """Console script for fastx_compressor."""
-    pass
+
 
 
 if __name__ == "__main__":
--- fastx_compressor/fastx_compressor.py
+++ fastx_compressor/fastx_compressor.py
@@ -1,22 +1,56 @@
 # -*- coding: utf-8 -*-
 
+
+
 """Main module."""
 
 "VERSION 1"
 
+
+
 def block_fastq_file(fastq_file, block_size=1000):
+    #globalish variables
+    BLOCK_LENGTH = 4
+    finishedString = ""
     blocks = ["", "", "", ""]
     counter = 0
+
+
     for line in fastq_file:
         blocks[counter % 4] += line
         counter += 1
+        #Check for when block size achieved
+        if (counter / BLOCK_LENGTH == block_size):
+            for temp in blocks:
+                finishedString += temp
+            
+            finishedString = "Total in block: " + str(block_size) +"\n" + finishedString
+
+            #yield block and reset variables for next block
+            yield finishedString
+            finishedString = ""
+            blocks = ["", "", "", ""]
+            counter = 0
+
             
 
-    finishedString = ""
+    #build final incomplete block
     for temp in blocks:
         finishedString += temp
+    totalSize = counter / BLOCK_LENGTH
+
+    totalSize = int(totalSize)
+
+    totalSize = str(totalSize)
+    totalSize = "Total in block: " + totalSize + "\n"
+    finishedString = totalSize + finishedString
 
 
 
 
-    return finishedString
+
+    yield finishedString
+
+
+def deblock_fastq_file(fastq_file, block_size=1000):
+    blocks = ["", "", "", ""]
